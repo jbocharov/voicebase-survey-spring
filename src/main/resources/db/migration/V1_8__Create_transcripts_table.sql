efe4bbbae288e116a85bc00d2968b34c93eb47d9
jbocharov <john@voicebase.com>
2017-05-16 09:06:07 -0700

    Add transcript_text column to create transcripts table script

--- src/main/resources/db/migration/V1_8__Create_transcripts_table.sql
+++ src/main/resources/db/migration/V1_8__Create_transcripts_table.sql
@@ -2,5 +2,6 @@ create table transcripts (
     id SERIAL UNIQUE not null PRIMARY KEY,
     media_id int REFERENCES media (id) ON DELETE CASCADE,
     vocabulary_id int REFERENCES vocabularies (id) ON DELETE CASCADE,
+    transcript_text text not null,
     date timestamp not null
 );
